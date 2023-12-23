bd0d5b119aa807d2120fea727b4006a7f7bae53f
chansule92 <chansule92@naver.com>
2023-12-23 20:12:29 +0900

    add_css_file

--- event.py
+++ event.py
@@ -111,3 +111,47 @@ def wishlist(temp):
     SALE_AMT = ' '
     LOAD_DTTM = temp[18]
     return [MSG_ID, MSG_NAME, CUST_ID, CNTNR_ID, VSTR_ID, COOKIE_ID, RFFR_URL,CONN_DEVICE_CD, SITE_EVNT_ID, SEARCH_WORD, PRDT_CD, CATE_CD, BRAND_CD, PRDT_QTY, BASKET_NO, NORM_SALE_AMT, ORD_NO, SALE_AMT, LOAD_DTTM]
+
+def order(temp):
+    MSG_ID = 'MG_ORDER'
+    MSG_NAME = '주문'
+    CUST_ID = temp[2]
+    CNTNR_ID = temp[3]
+    VSTR_ID = temp[4]
+    COOKIE_ID = temp[5]
+    RFFR_URL = temp[6]
+    CONN_DEVICE_CD = temp[7]
+    SITE_EVNT_ID = ' '
+    SEARCH_WORD = ' '
+    PRDT_CD = 'N30147'
+    CATE_CD = 'NEF'
+    BRAND_CD = 'NEO'
+    PRDT_QTY = 1
+    BASKET_NO = ''
+    NORM_SALE_AMT = 15000
+    ORD_NO = 'ORD000001'
+    SALE_AMT = 12000
+    LOAD_DTTM = temp[18]
+    return [MSG_ID, MSG_NAME, CUST_ID, CNTNR_ID, VSTR_ID, COOKIE_ID, RFFR_URL,CONN_DEVICE_CD, SITE_EVNT_ID, SEARCH_WORD, PRDT_CD, CATE_CD, BRAND_CD, PRDT_QTY, BASKET_NO, NORM_SALE_AMT, ORD_NO, SALE_AMT, LOAD_DTTM]
+
+def event(temp):
+    MSG_ID = 'MG_EVENT'
+    MSG_NAME = '이벤트'
+    CUST_ID = temp[2]
+    CNTNR_ID = temp[3]
+    VSTR_ID = temp[4]
+    COOKIE_ID = temp[5]
+    RFFR_URL = temp[6]
+    CONN_DEVICE_CD = temp[7]
+    SITE_EVNT_ID = 'EVENT001'
+    SEARCH_WORD = ' '
+    PRDT_CD = ''
+    CATE_CD = ''
+    BRAND_CD = ''
+    PRDT_QTY = ''
+    BASKET_NO = ''
+    NORM_SALE_AMT = ''
+    ORD_NO = ''
+    SALE_AMT = ''
+    LOAD_DTTM = temp[18]
+    return [MSG_ID, MSG_NAME, CUST_ID, CNTNR_ID, VSTR_ID, COOKIE_ID, RFFR_URL,CONN_DEVICE_CD, SITE_EVNT_ID, SEARCH_WORD, PRDT_CD, CATE_CD, BRAND_CD, PRDT_QTY, BASKET_NO, NORM_SALE_AMT, ORD_NO, SALE_AMT, LOAD_DTTM]
