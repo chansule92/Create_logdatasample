6e733e532ec675b81b65b60a5d7bb03337403afc
chansule92 <chansule92@naver.com>
2023-12-24 00:54:52 +0900

    eve_start

--- event.py
+++ event.py
@@ -1,3 +1,11 @@
+import random
+import pandas as pd
+WORD_LIST=pd.read_csv('SEARCH_WORD.csv')
+WORD_LIST=WORD_LIST[['word']].values.tolist()
+PRDT_MST=pd.read_csv('PRDT_MST.csv')
+PRDT_MST = PRDT_MST[['PRDT_CD','CATE_CD','BRAND_CD','PRDT_NAME','SALE_PRICE']].head().values.tolist()
+
+
 def login(temp):
     MSG_ID = 'MG_LOGIN'
     MSG_NAME = '로그인'
@@ -21,6 +29,7 @@ def login(temp):
     return [MSG_ID, MSG_NAME, CUST_ID, CNTNR_ID, VSTR_ID, COOKIE_ID, RFFR_URL,CONN_DEVICE_CD, SITE_EVNT_ID, SEARCH_WORD, PRDT_CD, CATE_CD, BRAND_CD, PRDT_QTY, BASKET_NO, NORM_SALE_AMT, ORD_NO, SALE_AMT, LOAD_DTTM]
 
 
+
 def search(temp):
     MSG_ID = 'MG_SEARCH'
     MSG_NAME = '상품검색'
@@ -31,7 +40,7 @@ def search(temp):
     RFFR_URL = temp[6]
     CONN_DEVICE_CD = temp[7]
     SITE_EVNT_ID = ' '
-    SEARCH_WORD = '겨울패딩'
+    SEARCH_WORD = random.choice(WORD_LIST)[0]
     PRDT_CD = ' '
     CATE_CD = ' '
     BRAND_CD = ' '
@@ -43,8 +52,9 @@ def search(temp):
     LOAD_DTTM = temp[18]
     return [MSG_ID, MSG_NAME, CUST_ID, CNTNR_ID, VSTR_ID, COOKIE_ID, RFFR_URL,CONN_DEVICE_CD, SITE_EVNT_ID, SEARCH_WORD, PRDT_CD, CATE_CD, BRAND_CD, PRDT_QTY, BASKET_NO, NORM_SALE_AMT, ORD_NO, SALE_AMT, LOAD_DTTM]
 
-
 def click(temp):
+    if temp[10] == ' ':
+        PRDT_CD = random.choice(PRDT_MST)[0]
     MSG_ID = 'MG_PRODUCT_CLICK'
     MSG_NAME = '상품탐색'
     CUST_ID = temp[2]
@@ -55,9 +65,9 @@ def click(temp):
     CONN_DEVICE_CD = temp[7]
     SITE_EVNT_ID = ' '
     SEARCH_WORD = ' '
-    PRDT_CD = 'N30147'
-    CATE_CD = 'NEF'
-    BRAND_CD = 'NEO'
+    PRDT_CD = PRDT_CD
+    CATE_CD = PRDT_MST[PRDT_MST['PRDT_CD']==PRDT_CD]['CATE_CD'][0]
+    BRAND_CD = PRDT_MST[PRDT_MST['PRDT_CD']==PRDT_CD]['BRAND_CD'][0]
     PRDT_QTY = ' '
     BASKET_NO = ' '
     NORM_SALE_AMT = ' '
@@ -65,7 +75,6 @@ def click(temp):
     SALE_AMT = ' '
     LOAD_DTTM = temp[18]
     return [MSG_ID, MSG_NAME, CUST_ID, CNTNR_ID, VSTR_ID, COOKIE_ID, RFFR_URL,CONN_DEVICE_CD, SITE_EVNT_ID, SEARCH_WORD, PRDT_CD, CATE_CD, BRAND_CD, PRDT_QTY, BASKET_NO, NORM_SALE_AMT, ORD_NO, SALE_AMT, LOAD_DTTM]
-
 
 def basket(temp):
     MSG_ID = 'MG_BASKET'
--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import random
 import datetime
 import pandas as pd
 
+RFFR_URL_LIST=pd.read_csv('RFFR_URL.csv')
+RFFR_URL_LIST=RFFR_URL_LIST[['url']].values.tolist()
+CUST_LIST=pd.read_csv('CUST_LIST.csv')
+CUST_LIST = CUST_LIST[['VSTR_ID','COOKIE_ID','CUST_ID']].head()
+DEVICE_LIST=pd.read_csv('DEVICE.csv')
+DEVICE_LIST=DEVICE_LIST[['device']].values.tolist()
 hour = random.randint(0, 23)
 minute = random.randint(0, 59)
 second = random.randint(0, 59)
@@ -9,23 +15,13 @@ now = datetime.datetime.now()
 rand_time = datetime.datetime(now.year, now.month, now.day, hour, minute, second)
 TIME=rand_time.strftime("%Y%m%d%H%M%S")
 
-RFFR_URL_LIST=pd.read_csv('RFFR_URL.csv')
-RFFR_URL_LIST=RFFR_URL_LIST[['url']].values.tolist()
-CUST_LIST=pd.read_csv('CUST_LIST.csv')
-CUST_LIST = CUST_LIST[['VSTR_ID','COOKIE_ID','CUST_ID']]
-DEVICE_LIST=pd.read_csv('DEVICE.csv')
-DEVICE_LIST=DEVICE_LIST[['device']].values.tolist()
-
 CNTNR_ID = 'website'
 ID = CUST_LIST['CUST_ID'][0]
 VSTR_ID=CUST_LIST['COOKIE_ID'][0]
 COOKIE_ID = CUST_LIST['COOKIE_ID'][0]
 RFFR_URL = random.choice(RFFR_URL_LIST)[0]
 CONN_DEVICE_CD = random.choice(DEVICE_LIST)[0]
-
-
 temp=['','',ID,CNTNR_ID,VSTR_ID, COOKIE_ID, RFFR_URL,CONN_DEVICE_CD,'','','','','','','','','','',TIME]
-
 import event
 event_list=[]
 event_list.append(event.login(temp))
@@ -33,7 +29,23 @@ event_list.append(event.login(temp))
 
 event_list.append(event.search(event_list[0]))
 event_list.append(event.click(event_list[1]))
-event_list.append(event.basket(event_list[2]))
+event_list.append(event.search(event_list[2]))
+print(event_list)
 
 
-print(RFFR_URL)
+
+"""for i in range(0,len(CUST_LIST)):
+    VSTR_ID=CUST_LIST.iloc[i][0]
+    COOKIE_ID=CUST_LIST.iloc[i][1]
+    CUST_ID=CUST_LIST.iloc[i][2]
+    RFFR_URL = random.choice(RFFR_URL_LIST)[0]
+    CONN_DEVICE_CD = random.choice(DEVICE_LIST)[0]
+    hour = random.randint(0, 23)
+    minute = random.randint(0, 59)
+    second = random.randint(0, 59)
+    now = datetime.datetime.now()
+    rand_time = datetime.datetime(now.year, now.month, now.day, hour, minute, second)
+    TIME=rand_time.strftime("%Y%m%d%H%M%S")
+    temp=['','',ID,CNTNR_ID,VSTR_ID, COOKIE_ID, RFFR_URL,CONN_DEVICE_CD,'','','','','','','','','','',TIME]
+
+"""
