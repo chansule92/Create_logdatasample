86682132295eed2b66954ea4e012d3af9dc77240
chansule92 <chansule92@naver.com>
2023-12-26 16:41:39 +0900

    ' ' -> ''

--- event.py
+++ event.py
@@ -9,8 +9,7 @@ PRDT_MST = PRDT_MST[['PRDT_CD','CATE_CD','BRAND_CD','PRDT_NAME','SALE_PRICE']]
 PRDT_MST_LIST = PRDT_MST[['PRDT_CD','CATE_CD','BRAND_CD','PRDT_NAME','SALE_PRICE']].values.tolist()
 def lpad(i, width, fillchar='0'):
     return str(i).rjust(width, fillchar)
-i=0
-j=0
+
 def login(temp):
     MSG_ID = 'MG_LOGIN'
     MSG_NAME = '로그인'
@@ -20,16 +19,16 @@ def login(temp):
     COOKIE_ID = temp[5]
     RFFR_URL = temp[6]
     CONN_DEVICE_CD = temp[7]
-    SITE_EVNT_ID = ' '
-    SEARCH_WORD = ' '
-    PRDT_CD = ' '
-    CATE_CD = ' '
-    BRAND_CD = ' '
-    PRDT_QTY = ' '
-    BASKET_NO = ' '
-    NORM_SALE_AMT = ' '
-    ORD_NO = ' '
-    SALE_AMT = ' '
+    SITE_EVNT_ID = ''
+    SEARCH_WORD = ''
+    PRDT_CD = ''
+    CATE_CD = ''
+    BRAND_CD = ''
+    PRDT_QTY = ''
+    BASKET_NO = ''
+    NORM_SALE_AMT = ''
+    ORD_NO = ''
+    SALE_AMT = ''
     LOAD_DTTM = temp[18]
     return [MSG_ID, MSG_NAME, CUST_ID, CNTNR_ID, VSTR_ID, COOKIE_ID, RFFR_URL,CONN_DEVICE_CD, SITE_EVNT_ID, SEARCH_WORD, PRDT_CD, CATE_CD, BRAND_CD, PRDT_QTY, BASKET_NO, NORM_SALE_AMT, ORD_NO, SALE_AMT, LOAD_DTTM]
 
@@ -44,16 +43,16 @@ def search(temp):
     COOKIE_ID = temp[5]
     RFFR_URL = temp[6]
     CONN_DEVICE_CD = temp[7]
-    SITE_EVNT_ID = ' '
+    SITE_EVNT_ID = ''
     SEARCH_WORD = random.choice(WORD_LIST)[0]
-    PRDT_CD = ' '
-    CATE_CD = ' '
-    BRAND_CD = ' '
-    PRDT_QTY = ' '
-    BASKET_NO = ' '
-    NORM_SALE_AMT = ' '
-    ORD_NO = ' '
-    SALE_AMT = ' '
+    PRDT_CD = ''
+    CATE_CD = ''
+    BRAND_CD = ''
+    PRDT_QTY = ''
+    BASKET_NO = ''
+    NORM_SALE_AMT = ''
+    ORD_NO = ''
+    SALE_AMT = ''
     LOAD_DTTM = temp[18]
     return [MSG_ID, MSG_NAME, CUST_ID, CNTNR_ID, VSTR_ID, COOKIE_ID, RFFR_URL,CONN_DEVICE_CD, SITE_EVNT_ID, SEARCH_WORD, PRDT_CD, CATE_CD, BRAND_CD, PRDT_QTY, BASKET_NO, NORM_SALE_AMT, ORD_NO, SALE_AMT, LOAD_DTTM]
 
@@ -69,53 +68,49 @@ def click(temp):
     COOKIE_ID = temp[5]
     RFFR_URL = temp[6]
     CONN_DEVICE_CD = temp[7]
-    SITE_EVNT_ID = ' '
-    SEARCH_WORD = ' '
+    SITE_EVNT_ID = ''
+    SEARCH_WORD = ''
     PRDT_CD = random.choice(PRDT_MST_LIST)[0]
     CATE_CD = PRDT_MST[PRDT_MST['PRDT_CD']==PRDT_CD]['CATE_CD'].iloc[0]
     BRAND_CD = PRDT_MST[PRDT_MST['PRDT_CD']==PRDT_CD]['BRAND_CD'].iloc[0]
-    PRDT_QTY = ' '
-    BASKET_NO = ' '
-    NORM_SALE_AMT = ' '
-    ORD_NO = ' '
-    SALE_AMT = ' '
+    PRDT_QTY = ''
+    BASKET_NO = ''
+    NORM_SALE_AMT = ''
+    ORD_NO = ''
+    SALE_AMT = ''
     LOAD_DTTM = temp[18]
     return [MSG_ID, MSG_NAME, CUST_ID, CNTNR_ID, VSTR_ID, COOKIE_ID, RFFR_URL,CONN_DEVICE_CD, SITE_EVNT_ID, SEARCH_WORD, PRDT_CD, CATE_CD, BRAND_CD, PRDT_QTY, BASKET_NO, NORM_SALE_AMT, ORD_NO, SALE_AMT, LOAD_DTTM]
 
-def basket(temp):
-    global i
-    i=i+1
-    a=random.choice([1,1,1,2,3])
-    for k in range(0,a):
-        if temp[10] == ' ':
-            PRDT_CD = random.choice(PRDT_MST_LIST)[0]
-        elif k != 0:
-            PRDT_CD = random.choice(PRDT_MST_LIST)[0]
-        else:
-            PRDT_CD = temp[10]
-        MSG_ID = 'MG_BASKET'
-        MSG_NAME = '장바구니'
-        CUST_ID = temp[2]
-        CNTNR_ID = temp[3]
-        VSTR_ID = temp[4]
-        COOKIE_ID = temp[5]
-        RFFR_URL = temp[6]
-        CONN_DEVICE_CD = temp[7]
-        SITE_EVNT_ID = ' '
-        SEARCH_WORD = ' '
-        CATE_CD = PRDT_MST[PRDT_MST['PRDT_CD']==PRDT_CD]['CATE_CD'].iloc[0]
-        BRAND_CD = PRDT_MST[PRDT_MST['PRDT_CD']==PRDT_CD]['BRAND_CD'].iloc[0]
-        PRDT_QTY = random.randrange(1,5)
-        BASKET_NO = 'BSK'+lpad(i,6,'0')
-        NORM_SALE_AMT = 15000
-        ORD_NO = ' '
-        SALE_AMT = ' '
-        LOAD_DTTM = temp[18]
+def basket(temp,bsk_number,k):
+    if temp[10] == '':
+        PRDT_CD = random.choice(PRDT_MST_LIST)[0]
+    elif k != 0:
+        PRDT_CD = random.choice(PRDT_MST_LIST)[0]
+    else:
+        PRDT_CD = temp[10]
+    MSG_ID = 'MG_BASKET'
+    MSG_NAME = '장바구니'
+    CUST_ID = temp[2]
+    CNTNR_ID = temp[3]
+    VSTR_ID = temp[4]
+    COOKIE_ID = temp[5]
+    RFFR_URL = temp[6]
+    CONN_DEVICE_CD = temp[7]
+    SITE_EVNT_ID = ''
+    SEARCH_WORD = ''
+    CATE_CD = PRDT_MST[PRDT_MST['PRDT_CD']==PRDT_CD]['CATE_CD'].iloc[0]
+    BRAND_CD = PRDT_MST[PRDT_MST['PRDT_CD']==PRDT_CD]['BRAND_CD'].iloc[0]
+    PRDT_QTY = random.randrange(1,5)
+    BASKET_NO = 'BSK'+lpad(bsk_number,6,'0')
+    NORM_SALE_AMT = 15000
+    ORD_NO = ''
+    SALE_AMT = ''
+    LOAD_DTTM = temp[18]
     return [MSG_ID, MSG_NAME, CUST_ID, CNTNR_ID, VSTR_ID, COOKIE_ID, RFFR_URL,CONN_DEVICE_CD, SITE_EVNT_ID, SEARCH_WORD, PRDT_CD, CATE_CD, BRAND_CD, PRDT_QTY, BASKET_NO, NORM_SALE_AMT, ORD_NO, SALE_AMT, LOAD_DTTM]
 
 
 def wishlist(temp):
-    if temp[10] == ' ':
+    if temp[10] == '':
         PRDT_CD = random.choice(PRDT_MST_LIST)[0]
     else :
         PRDT_CD = temp[10]
@@ -127,48 +122,45 @@ def wishlist(temp):
     COOKIE_ID = temp[5]
     RFFR_URL = temp[6]
     CONN_DEVICE_CD = temp[7]
-    SITE_EVNT_ID = ' '
-    SEARCH_WORD = ' '
+    SITE_EVNT_ID = ''
+    SEARCH_WORD = ''
     CATE_CD = PRDT_MST[PRDT_MST['PRDT_CD']==PRDT_CD]['CATE_CD'].iloc[0]
     BRAND_CD = PRDT_MST[PRDT_MST['PRDT_CD']==PRDT_CD]['BRAND_CD'].iloc[0]
     PRDT_QTY = 1
     BASKET_NO = ''
     NORM_SALE_AMT = PRDT_MST[PRDT_MST['PRDT_CD']==PRDT_CD]['SALE_PRICE'].iloc[0]
-    ORD_NO = ' '
-    SALE_AMT = ' '
+    ORD_NO = ''
+    SALE_AMT = ''
     LOAD_DTTM = temp[18]
     return [MSG_ID, MSG_NAME, CUST_ID, CNTNR_ID, VSTR_ID, COOKIE_ID, RFFR_URL,CONN_DEVICE_CD, SITE_EVNT_ID, SEARCH_WORD, PRDT_CD, CATE_CD, BRAND_CD, PRDT_QTY, BASKET_NO, NORM_SALE_AMT, ORD_NO, SALE_AMT, LOAD_DTTM]
 
-def order(temp):
-    global j
-    j=j+1
-    a0=random.choice([1,1,2,3])
-    for k in range(0,a0):
-        if temp[10] == ' ':
-            PRDT_CD = random.choice(PRDT_MST_LIST)[0]
-        elif k != 0:
-            PRDT_CD = random.choice(PRDT_MST_LIST)[0]
-        else:
-            PRDT_CD = temp[10]
-        MSG_ID = 'MG_ORDER'
-        MSG_NAME = '주문'
-        CUST_ID = temp[2]
-        CNTNR_ID = temp[3]
-        VSTR_ID = temp[4]
-        COOKIE_ID = temp[5]
-        RFFR_URL = temp[6]
-        CONN_DEVICE_CD = temp[7]
-        SITE_EVNT_ID = ' '
-        SEARCH_WORD = ' '
-        CATE_CD = PRDT_MST[PRDT_MST['PRDT_CD']==PRDT_CD]['CATE_CD'].iloc[0]
-        BRAND_CD = PRDT_MST[PRDT_MST['PRDT_CD']==PRDT_CD]['BRAND_CD'].iloc[0]
-        PRDT_QTY = random.randrange(1,5)
-        BASKET_NO = ''
-        NORM_SALE_AMT = PRDT_MST[PRDT_MST['PRDT_CD']==PRDT_CD]['SALE_PRICE'].iloc[0]
-        ORD_NO = 'ORD'+lpad(i,6,'0')
-        SALE_AMT = (PRDT_MST[PRDT_MST['PRDT_CD']==PRDT_CD]['SALE_PRICE'].iloc[0]) * 0.8
-        LOAD_DTTM = temp[18]
+def order(temp,ord_number,k):
+    if temp[10] == '':
+        PRDT_CD = random.choice(PRDT_MST_LIST)[0]
+    elif k != 0:
+        PRDT_CD = random.choice(PRDT_MST_LIST)[0]
+    else:
+        PRDT_CD = temp[10]
+    MSG_ID = 'MG_ORDER'
+    MSG_NAME = '주문'
+    CUST_ID = temp[2]
+    CNTNR_ID = temp[3]
+    VSTR_ID = temp[4]
+    COOKIE_ID = temp[5]
+    RFFR_URL = temp[6]
+    CONN_DEVICE_CD = temp[7]
+    SITE_EVNT_ID = ''
+    SEARCH_WORD = ''
+    CATE_CD = PRDT_MST[PRDT_MST['PRDT_CD']==PRDT_CD]['CATE_CD'].iloc[0]
+    BRAND_CD = PRDT_MST[PRDT_MST['PRDT_CD']==PRDT_CD]['BRAND_CD'].iloc[0]
+    PRDT_QTY = random.randrange(1,5)
+    BASKET_NO = ''
+    NORM_SALE_AMT = PRDT_MST[PRDT_MST['PRDT_CD']==PRDT_CD]['SALE_PRICE'].iloc[0]
+    ORD_NO = 'ORD'+lpad(ord_number,6,'0')
+    SALE_AMT = (PRDT_MST[PRDT_MST['PRDT_CD']==PRDT_CD]['SALE_PRICE'].iloc[0]) * 0.8
+    LOAD_DTTM = temp[18]
     return [MSG_ID, MSG_NAME, CUST_ID, CNTNR_ID, VSTR_ID, COOKIE_ID, RFFR_URL,CONN_DEVICE_CD, SITE_EVNT_ID, SEARCH_WORD, PRDT_CD, CATE_CD, BRAND_CD, PRDT_QTY, BASKET_NO, NORM_SALE_AMT, ORD_NO, SALE_AMT, LOAD_DTTM]
+
 
 def event(temp):
     MSG_ID = 'MG_EVENT'
@@ -180,7 +172,7 @@ def event(temp):
     RFFR_URL = temp[6]
     CONN_DEVICE_CD = temp[7]
     SITE_EVNT_ID = random.choice(SITE_EVENT_LIST)[0]
-    SEARCH_WORD = ' '
+    SEARCH_WORD = ''
     PRDT_CD = ''
     CATE_CD = ''
     BRAND_CD = ''
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import random
 import datetime
 import pandas as pd
 
-RFFR_URL_LIST=pd.read_csv('RFFR_URL.csv')
+RFFR_URL_LIST=pd.read_csv('RFFR_URL.csv',low_memory=False)
 RFFR_URL_LIST=RFFR_URL_LIST[['url']].values.tolist()
-CUST_LIST=pd.read_csv('CUST_LIST.csv')
-CUST_LIST = CUST_LIST[['VSTR_ID','COOKIE_ID','CUST_ID']].head()
-DEVICE_LIST=pd.read_csv('DEVICE.csv')
+CUST_LIST=pd.read_csv('CUST_LIST.csv',low_memory=False)
+CUST_LIST = CUST_LIST[['VSTR_ID','COOKIE_ID','CUST_ID']].head(1)
+DEVICE_LIST=pd.read_csv('DEVICE.csv',low_memory=False)
 DEVICE_LIST=DEVICE_LIST[['device']].values.tolist()
 hour = random.randint(0, 23)
 minute = random.randint(0, 59)
@@ -16,28 +16,16 @@ rand_time = datetime.datetime(now.year, now.month, now.day, hour, minute, second
 TIME=rand_time.strftime("%Y%m%d%H%M%S")
 
 CNTNR_ID = 'website'
-ID = CUST_LIST['CUST_ID'][0]
-VSTR_ID=CUST_LIST['COOKIE_ID'][0]
-COOKIE_ID = CUST_LIST['COOKIE_ID'][0]
-RFFR_URL = random.choice(RFFR_URL_LIST)[0]
-CONN_DEVICE_CD = random.choice(DEVICE_LIST)[0]
-temp=['','',ID,CNTNR_ID,VSTR_ID, COOKIE_ID, RFFR_URL,CONN_DEVICE_CD,'','','','','','','','','','',TIME]
 import event
 event_list=[]
-event_list.append(event.login(temp))
-event_list.append(event.search(event_list[0]))
-event_list.append(event.click(event_list[1]))
-event_list.append(event.basket(event_list[2]))
-event_list.append(event.wishlist(event_list[3]))
-event_list.append(event.order(event_list[4]))
-event_list.append(event.order(event_list[5]))
-event_list.append(event.basket(event_list[6]))
-event_list.append(event.event(event_list[7]))
-print(event_list)
 
+"""
+로그인
+"""
+bsk_number=0
+ord_number=0
 
-
-"""for i in range(0,len(CUST_LIST)):
+for i in range(0,len(CUST_LIST)):
     VSTR_ID=CUST_LIST.iloc[i][0]
     COOKIE_ID=CUST_LIST.iloc[i][1]
     CUST_ID=CUST_LIST.iloc[i][2]
@@ -49,6 +37,11 @@ print(event_list)
     now = datetime.datetime.now()
     rand_time = datetime.datetime(now.year, now.month, now.day, hour, minute, second)
     TIME=rand_time.strftime("%Y%m%d%H%M%S")
-    temp=['','',ID,CNTNR_ID,VSTR_ID, COOKIE_ID, RFFR_URL,CONN_DEVICE_CD,'','','','','','','','','','',TIME]
-
-"""
+    temp=['','',CUST_ID,CNTNR_ID,VSTR_ID, COOKIE_ID, RFFR_URL,CONN_DEVICE_CD,'','','','','','','','','','',TIME]
+    
+    event_list.append(event.login(temp))
+    """temp[18]=temp[18]+시간더하기"""
+    event_list.append(event.order(temp,1,0))
+    """temp[18]=temp[18]+시간더하기"""
+    event_list.append(event.order(temp,1,1))
+print(event_list)
